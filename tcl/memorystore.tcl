b54f34853790357a1c97929049cad25459609c23
Neofytos Dimitriou <neo@jerily.cy>
2023-10-31 03:00:39 -0400

    modified to use tsv array i.e. data shared between threads

--- tcl/memorystore.tcl
+++ tcl/memorystore.tcl
@@ -2,15 +2,16 @@
 # SPDX-FileCopyrightText: 2023 Neofytos Dimitriou (neo@jerily.cy)
 # SPDX-License-Identifier: MIT.
 
+package require Thread
+
 namespace eval ::tsession::MemoryStore {
-    array set sessions {}
+    tsv::array set sessions {}
 
     proc retrieve_session {session_id} {
-        variable sessions
 
-        if { [info exists sessions(${session_id})] } {
+        if { [tsv::exists sessions ${session_id}] } {
 
-            set session $sessions(${session_id})
+            set session [tsv::get sessions ${session_id}]
 
             # check if "session" expired
             set expires [dict get ${session} expires]
@@ -26,20 +27,16 @@ namespace eval ::tsession::MemoryStore {
     }
 
     proc save_session {session_id session_dict} {
-        variable sessions
-        set sessions(${session_id}) $session_dict
+        tsv::set sessions ${session_id} $session_dict
     }
 
     proc destroy_session {session_id} {
-        variable sessions
-        if { [info exists sessions(${session_id})] } {
-            unset sessions(${session_id})
+        if { [tsv::exists sessions ${session_id}] } {
+            tsv::unset sessions ${session_id}
         }
     }
 
     proc touch_session {session_id session_dict} {
-        variable sessions
-
         set current_session_dict [retrieve_session ${session_id}]
         if { ${current_session_dict} ne {} } {
             dict set current_session_dict expires [dict get ${session_dict} expires]
